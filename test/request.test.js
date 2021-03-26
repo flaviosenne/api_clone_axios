47a2ef82548df69e938c55aa948244af100fa581
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-25 22:48:04 -0300

    structure file test request

--- test/request.test.js
+++ test/request.test.js
@@ -1,7 +1,21 @@
-const {describe, it} =require('mocha')
+const {describe, it, before, afterEach} =require('mocha')
 const assert = require('assert')
+const Request = require('../src/request')
+const {createSandbox} = require('sinon')
 
-describe('#test', () => {
+
+describe('Request helpers', () => {
+    let sandbox
+    let request
+    
+    before(() => {
+        sandbox = createSandbox()
+        request = new Request()
+    })
+
+    afterEach(() => sandbox.restore())
+
+
     it('shoud test', () => {
         assert.ok(true)
     })
